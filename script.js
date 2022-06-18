39a38d07d94d8878032c45651665ea25d80996fb
owentr1369 <tamvaa1306@gmail.com>
2022-06-18 22:44:03 +0700

    filter method

---
+++ script.js
@@ -0,0 +1,13 @@
+const items = [
+  { name: "Bike", price: 200 },
+  { name: "TV", price: 300 },
+  { name: "Album", price: 50 },
+  { name: "Book", price: 20 },
+  { name: "Keyboard", price: 150 },
+];
+
+// 1. Filter method
+// const filterArray = items.filter((item) => {
+//   return item.price > 100;
+// });
+// console.log(filterArray);
